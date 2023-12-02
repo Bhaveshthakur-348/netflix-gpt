eb45f5e51f8a2d9747c808a5a6aaec6d7d6256df
Bhavesh Thakur <102952918+bhavesh-348@users.noreply.github.com>
2023-12-02 22:19:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="font-bold bg-red-300">
+        <p>Netflix gpt</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
