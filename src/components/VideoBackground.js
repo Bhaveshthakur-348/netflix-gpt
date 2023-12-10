09457ce2ae29cb59338b35c4b858404bf74a4729
Bhavesh Thakur <102952918+bhavesh-348@users.noreply.github.com>
2023-12-10 10:42:42 +0530

    Add: videobackground and store in slice addTrailerVideo

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,40 @@
+import { useEffect } from "react"
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
 const VideoBackground = ({movieId}) => {
-  console.log(movieId)
+
+  const trailerVideo = useSelector(store => store.movies?.trailerVideo)
+  const dispatch = useDispatch();
+
+  const getMoviesVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    )
+
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type === "Trailer")
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer))
+  }
+
+  useEffect(() => {
+    getMoviesVideos();
+  }, [])
   return (
-    <div>VideoBackground</div>
+    <div className=" w-screen">
+      <iframe 
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      ></iframe>
+    </div>
   )
 }
 
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -11,9 +11,12 @@ const moviesSlice = createSlice({
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
