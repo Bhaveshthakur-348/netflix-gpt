37c8b57288482f71923ae5d3c71b0c049d0eb60c
Bhavesh Thakur <64855358+Bhaveshthakur-348@users.noreply.github.com>
2023-12-25 22:01:48 +0530

    API key securing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,14 +5,15 @@ export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
 export const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDZlYTk1ODc1ZjcwNjMzNGVkY2ZlYTIwNDhkY2E3MSIsInN1YiI6IjY1NmNjNDlmNGE0YmY2MDEwMzUwN2Q0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QrVBaLX4foSc_NI7c414C90GnpEyUjoziP8W5-LPtbg'
-  }
+    accept: "application/json",
+    Authorization:
+      "Bearer " + process.env.REACT_APP_TMDB_KEY,
+  },
 };
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";  
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
@@ -23,4 +24,5 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const GEMINI_KEY = "AIzaSyDwYqmnyn9i4GWc4Y6H8coHQbhagngu1CE"
+
+export const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
