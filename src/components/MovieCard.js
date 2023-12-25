105ee2f0de42055611d987c58b5548a555cecc46
Bhavesh Thakur <64855358+Bhaveshthakur-348@users.noreply.github.com>
2023-12-25 17:47:14 +0530

    movie card design change

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,8 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="w-36 md:w-48 pr-4 transition-transform transform-gpu hover:scale-110">
+      <img className="w-full h-auto" alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
   );
 };
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,19 @@
 import MovieCard from "./MovieCard";
+import "./MovieList.css";
 
 const MovieList = ({ title, movies }) => {
-    return (
-        <div className="px-6 ">
-          <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-          <div className="flex overflow-x-scroll">
-            <div className="flex">
-              {movies?.map((movie) => (
-                <MovieCard key={movie.id} posterPath={movie.poster_path} />
-              ))}
-            </div>
-          </div>
+  return (
+    <div className="px-6">
+      <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-scroll custom-scrollbar">
+        <div className="flex">
+          {movies?.map((movie) => (
+            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default MovieList
+export default MovieList;
