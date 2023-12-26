71ac0a06998872b05c995791cf5b1592600a1ee1
Bhavesh Thakur <64855358+Bhaveshthakur-348@users.noreply.github.com>
2023-12-27 00:45:00 +0530

    Top Rated movies added

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -5,12 +5,14 @@ import Header from "./Header"
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import GptSearch from "./GptSearch";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
 
 const Browse = () => {
   const showGptSearch = useSelector(store => store.gpt.showGptSearch)
   
   useNowPlayingMovies();
   usePopularMovies();
+  useTopRatedMovies();
   return (
     <div>
       <Header />
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -6,6 +6,7 @@ const moviesSlice = createSlice({
     nowPlayingMovies: null,
     popularMovies: null,
     trailerVideo: null,
+    topRatedMovies: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
@@ -17,9 +18,12 @@ const moviesSlice = createSlice({
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
     },
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTopRatedMovies } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
